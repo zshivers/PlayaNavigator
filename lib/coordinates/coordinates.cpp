b9a68423b84d7c6bdbe0cf30a42063e56db78ba1
Zack <zack@Zacks-MacBook-Pro.local>
2023-05-08 22:37:24 -0700

    Added coordinate library for converting lat/lon to playa map relative coordinates.

---
+++ lib/coordinates/coordinates.cpp
@@ -0,0 +1,107 @@
+#include "coordinates.h"
+
+#include <cmath>
+
+namespace {
+
+constexpr double PI = 3.14159265359;
+constexpr double TWO_PI = 6.28318530718;
+double radToDeg(const double radians) { return radians * (180.0 / PI); }
+double degToRad(const double deg) { return deg * (PI / 180.0); }
+constexpr double feetToMeters(const double feet) { return feet / 3.281; }
+inline double square(double in) { return in * in; }
+
+int RoundUpToNearestMultiple(int in, int multiple) {
+  if (multiple == 0) return in;
+
+  int remainder = in % multiple;
+  if (remainder == 0) return in;
+
+  return in + multiple - remainder;
+}
+
+double distanceBetween(double lat1, double long1, double lat2, double long2) {
+  // returns distance in meters between two positions, both specified
+  // as signed decimal-degrees latitude and longitude. Uses great-circle
+  // distance computation for hypothetical sphere of radius 6372795 meters.
+  // Because Earth is no exact sphere, rounding errors may be up to 0.5%.
+  // Courtesy of Maarten Lamers
+  constexpr double kEarthRadius = 6372795.0;
+  double delta = degToRad(long1 - long2);
+  double sdlong = sin(delta);
+  double cdlong = cos(delta);
+  lat1 = degToRad(lat1);
+  lat2 = degToRad(lat2);
+  const double slat1 = sin(lat1);
+  const double clat1 = cos(lat1);
+  const double slat2 = sin(lat2);
+  const double clat2 = cos(lat2);
+  delta = (clat1 * slat2) - (slat1 * clat2 * cdlong);
+  delta = square(delta);
+  delta += square(clat2 * sdlong);
+  delta = sqrt(delta);
+  double denom = (slat1 * slat2) + (clat1 * clat2 * cdlong);
+  delta = atan2(delta, denom);
+  return delta * kEarthRadius;
+}
+
+double courseTo(double lat1, double long1, double lat2, double long2) {
+  // returns course in degrees (North=0, West=-90, East=+90) from position 1 to
+  // position 2, both specified as signed decimal-degrees latitude and
+  // longitude. Because Earth is no exact sphere, calculated course may be off
+  // by a tiny fraction. Courtesy of Maarten Lamers
+  double dlon = degToRad(long2 - long1);
+  lat1 = degToRad(lat1);
+  lat2 = degToRad(lat2);
+  double y = sin(dlon) * cos(lat2);
+  double x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dlon);
+  return radToDeg(atan2(y, x));
+}
+}  // namespace
+
+PlayaCoords LatLonToPlayaCoords(const PlayaMapConfig& pmc, const LatLon& in) {
+  PlayaCoords out;
+  out.radius_m =
+      distanceBetween(pmc.center.lat, pmc.center.lon, in.lat, in.lon);
+  out.angle_deg = courseTo(pmc.center.lat, pmc.center.lon, in.lat, in.lon) +
+                  pmc.rotation_deg;
+  return out;
+}
+
+bool IsAddressable(const PlayaMapConfig& pmc, const PlayaCoords& pc) {
+  constexpr double angle_10_deg = (360.0 / 12) * -2.0;
+  constexpr double angle_2_deg = (360.0 / 12) * 2.0;
+  return pc.radius_m > pmc.esplanade_radius_m &&
+         pc.radius_m < pmc.last_road_radius_m &&
+         !(pc.angle_deg > angle_10_deg && pc.angle_deg < angle_2_deg);
+}
+
+std::optional<PlayaAddress> PlayaCoordsToAddress(const PlayaMapConfig& pmc,
+                                                 const PlayaCoords& pc) {
+  if (!IsAddressable(pmc, pc)) return std::nullopt;
+  PlayaAddress address;
+  double clock_decimal = pc.angle_deg / 360.0 * 12.0;
+  if (clock_decimal < 0.0) clock_decimal += 12.0;
+  address.hour = static_cast<uint8_t>(clock_decimal);
+  if (address.hour == 0) address.hour = 12;
+  address.minute = static_cast<uint8_t>((clock_decimal - address.hour) * 60);
+  address.minute =
+      RoundUpToNearestMultiple(address.minute, 5);  // Round to nearest 5 mins.
+  if (address.minute == 60) address.minute = 0;
+
+  constexpr double kBoundaryPadding_m = 50;
+  for (int i = 0; i < pmc.roads.size(); ++i) {
+    double r_before = i == 0 ? pmc.roads[0].radius_m - kBoundaryPadding_m
+                             : pmc.roads[i].radius_m;
+    double r_after = i < (pmc.roads.size() - 1)
+                         ? r_after = pmc.roads[i + 1].radius_m
+                         : r_after = r_before + kBoundaryPadding_m;
+    const double r_mid = (r_before + r_after) / 2.0;
+    if (pc.radius_m < r_mid) {
+      address.road = pmc.roads[i].road;
+      break;
+    }
+  }
+
+  return address;
+}
---
+++ lib/coordinates/coordinates.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <optional>
+
+// Latitude and Longitude in decimal format.
+// +Lat = N, -Lat = S
+// +Lon = E, -Lon = W
+struct LatLon {
+  double lat, lon;
+};
+
+// Important features that define the geometry of the map.
+struct PlayaMapConfig {
+  LatLon center;                // Central point all of BM is laid out around.
+  double rotation_deg;          // Rotation from North to 12:00.
+  double esplanade_radius_m;    // Most inner road.
+  double last_road_radius_m;    // Most outer road.
+  double trash_fence_radius_m;  // Radius of circle circumscribing trash fench
+                                // pentagon.
+  struct RoadConfig {
+    char road;        // Single letter for road.
+    double radius_m;  // Radius from the center.
+  };
+  std::array<RoadConfig, 12> roads;
+};
+
+// Coordinates relative to the center of the playa.
+// angle_deg ==   0 -> 12:00
+// angle_deg == +90 ->  3:00
+// angle_deg == -90 ->  9:00
+// Radius is the distance from the center point (the man).
+struct PlayaCoords {
+  double radius_m, angle_deg;
+};
+
+struct PlayaAddress {
+  uint8_t hour;
+  uint8_t minute;
+  char road;
+
+  friend bool operator==(const PlayaAddress& lhs, const PlayaAddress& rhs) {
+    return lhs.hour == rhs.hour && lhs.minute == rhs.minute &&
+           lhs.road == rhs.road;
+  }
+};
+
+PlayaCoords LatLonToPlayaCoords(const PlayaMapConfig& pmc, const LatLon& in);
+
+// Does the supplied PlayaCoords have an address?
+// The only parts of the playa that are addressable lie within an annulus
+// between Esplanade and the last road, and are not between 10 o'clock and 2
+// o'clock.
+bool IsAddressable(const PlayaMapConfig& pmc, const PlayaCoords& pc);
+
+std::optional<PlayaAddress> PlayaCoordsToAddress(const PlayaMapConfig& pmc,
+                                                 const PlayaCoords& pc);
