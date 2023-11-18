e0ec1ccbd04d194046b17a64132d75dbdd24c393
Zack Shivers <zachery.shivers@gmail.com>
2023-11-17 18:56:22 -0800

    Move toilet location script into script/

---
+++ firmware/main/scripts/toilet_locations/README.md
@@ -0,0 +1,4 @@
+The data from `Portable_Toilets.json` contains a rectangle for each toilet rather than a single lat/lon pair. This script outputs the centroid of each toilet.
+
+1. `npm install` to download dependencies.
+2. `node toilet_locations.js` - Outputs coordinates in a format that can be pasted into the config JSON file.
--- firmware/main/scripts/toilet_locations/toilet_locations.js
+++ firmware/main/scripts/toilet_locations/toilet_locations.js
@@ -7,5 +7,5 @@ var toilet_data = JSON.parse(content);
 for (let t in toilet_data["features"]) {
     let feature = toilet_data["features"][t]
     let centroid = turf.centroid(feature)["geometry"]
-    console.log("{" + centroid["coordinates"][1] + ", " + centroid["coordinates"][0] + "},")
+    console.log("[" + centroid["coordinates"][1] + ", " + centroid["coordinates"][0] + "],")
 }
